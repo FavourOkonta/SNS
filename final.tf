b0a21cf19e58176fc5d012223c7b67ab4e346130
Favour Okonta <HP@DESKTOP-4AVRTVM.ligmus360.com>
2020-10-14 12:39:27 +0100

    updated

--- final.tf
+++ final.tf
@@ -87,11 +87,11 @@ resource "aws_sns_topic" "sns" {
 }
 EOF
 
-  provisioner "local-exec" {
-    command = "aws sns subscribe --topic-arn 'arn:aws:sns:us-east-1:697430341089:email' --protocol email --notification-endpoint ${var.alarms_email}"
+  #provisioner "local-exec" {
+    #command = "aws sns subscribe --topic-arn 'arn:aws:sns:us-east-1:697430341089:email' --protocol email --notification-endpoint ${var.alarms_email}"
     #command = "aws sns subscribe --topic-arn   arn:aws:sns:eu-west-1:871994821053:my-test-alarms-topic --protocol email --notification-endpoint ${var.alarms_email}"
-  }
-}
+ # }
+#}
 
 resource "aws_lambda_function_event_invoke_config" "example" {
   function_name = aws_lambda_function.lambda.arn
