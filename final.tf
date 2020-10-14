bb7af869d6ea6b6f8bee7e3405eebd33df5b2d0b
Favour Okonta <HP@DESKTOP-4AVRTVM.ligmus360.com>
2020-10-14 12:44:20 +0100

    updated

--- final.tf
+++ final.tf
@@ -3,6 +3,13 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "favtech-bucket"
+    key    = "techbleat.tfstate"
+    region = "us-east-1"
+  }
+}
 
 resource "aws_lambda_function" "lambda" {
   filename         = "index.zip"
@@ -87,11 +94,11 @@ resource "aws_sns_topic" "sns" {
 }
 EOF
 
-  #provisioner "local-exec" {
-    #command = "aws sns subscribe --topic-arn 'arn:aws:sns:us-east-1:697430341089:email' --protocol email --notification-endpoint ${var.alarms_email}"
+  provisioner "local-exec" {
+    command = "aws sns subscribe --topic-arn 'arn:aws:sns:us-east-1:697430341089:email' --protocol email --notification-endpoint ${var.alarms_email}"
     #command = "aws sns subscribe --topic-arn   arn:aws:sns:eu-west-1:871994821053:my-test-alarms-topic --protocol email --notification-endpoint ${var.alarms_email}"
- # }
-#}
+  }
+}
 
 resource "aws_lambda_function_event_invoke_config" "example" {
   function_name = aws_lambda_function.lambda.arn
